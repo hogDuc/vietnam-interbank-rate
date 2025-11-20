639611d53592a0a36b9eb71768dd6c9e0c53cf3e
Duc Nguyen <114228174+hogDuc@users.noreply.github.com>
2025-11-20 10:49:21 +0700

    Inital commit

---
+++ interbank_crawler.py
@@ -0,0 +1,123 @@
+import requests
+import pandas as pd
+from io import StringIO
+from datetime import datetime
+from bs4 import BeautifulSoup
+import logging
+from logging.handlers import RotatingFileHandler
+from send_email import send_email
+from dotenv import load_dotenv
+import os
+
+# Load env variables
+load_dotenv()
+FILE_NAME = os.getenv('FILE_NAME')
+daily_url = os.getenv('DAILY_INTERBANK_URL')
+NOTI_EMAIL = os.getenv('NOTI_EMAIL')
+RECEIPANT = os.getenv('RECEIPANT')
+DATA_FOLDER = os.getenv('DATA_FOLDER')
+LOG_FOLDER = os.getenv('LOG_FOLDER')
+FPTS_PROXY = os.getenv('FPTS_PROXY')
+
+# File names
+file_path = os.path.join(DATA_FOLDER, FILE_NAME)
+log_file = os.path.join(LOG_FOLDER, "interbank-crawler.log")
+
+# Logging handler
+logger = logging.getLogger('interbank-crawler')
+logger.setLevel(logging.INFO)
+
+handler = RotatingFileHandler(
+    log_file, maxBytes=5_000_000, backupCount=5 , encoding='utf-8'
+)
+formatter = logging.Formatter(
+    '%(asctime)s | %(levelname)s | %(name)s | %(message)s',
+    datefmt="%Y-%m-%d %H:%M:%S"
+)
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+# Read old data
+old_data = pd.read_excel(file_path)
+latest_date = old_data['date'].max()
+
+# Request website
+
+proxies = {
+    "http": FPTS_PROXY,
+    "https": FPTS_PROXY
+}
+ 
+r = requests.get(daily_url, proxies=proxies)
+soup = BeautifulSoup(r.text, 'html.parser')
+
+table = soup.find_all("table", class_="bi01-table")
+date_str = soup.select_one('.bi01-subnote strong').get_text(strip=True)
+current_date = datetime.strptime(date_str, '%d/%m/%Y') # Date on the website
+
+# Crawler
+if current_date > latest_date:
+
+    logger.info(f"Updating data for {current_date.date()}...")
+    
+    try:
+
+        today_rate = pd.read_html(StringIO(str(table[1])), decimal='.', flavor='bs4', thousands=".")[0]
+        today_rate.columns = ['maturity', 'interbank_rate', 'value']
+        today_rate = today_rate.dropna().assign(
+            date = current_date,
+            value = lambda df: df['value']\
+                .str.replace(r"\(\*\)", "", regex=True).astype(str)\
+                    .apply(lambda x: x.replace(",", "", x.count(",") - 1))\
+                        .str.replace(",", ".").astype(float) * 1_000_000_000,
+            interbank_rate = lambda df: df['interbank_rate'].str.replace(r"\(\*\)", "", regex=True)\
+                .str.replace(",", '.').astype(float) / 100
+        )
+
+        updated_data = pd.concat([old_data, today_rate])
+        updated_data.to_excel(file_path, index=False)
+
+        logger.info(f"Successfully updated data for {current_date.date()}...")
+        html_res = today_rate.to_html(index=False, border=1)
+
+        # Send notification email
+        send_email(
+            sender=NOTI_EMAIL,
+            receiver=RECEIPANT,
+            subject=f"Interbank rate report | {current_date}",
+            body=f"""
+            <html><body>
+                <h3>Crawler Report for {current_date}</h3>
+                {html_res}
+            </body></html>
+            """
+        )
+
+    except Exception as error:
+        logger.exception(f"Error at {current_date}")
+        send_email(
+            sender=NOTI_EMAIL,
+            receiver=RECEIPANT,
+            subject=f"Interbank Crawler Failed | {current_date}",
+            body=f"""
+            <html><body>
+                <h3>Crawler failed on {current_date}</h3>
+                {error}
+            </body></html>
+            """
+        )
+
+else:
+    logger.info(f'Data is already updated')
+    
+    send_email(
+        sender=NOTI_EMAIL,
+        receiver=RECEIPANT,
+        subject=f"No new Data | {current_date}",
+        body=f"""
+        <html><body>
+            <h3>Crawler Report on {current_date}</h3>
+            Data is either already present in the database, or it's not updated on the official website. Please review
+        </body></html>
+        """
+    )
---
+++ send_email.py
@@ -0,0 +1,83 @@
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+import google.auth
+import json
+import os
+import base64
+import requests
+import json
+from google.oauth2.credentials import Credentials
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from dotenv import load_dotenv
+
+load_dotenv()
+FPTS_PROXY = os.getenv('FPTS_PROXY')
+
+
+SCOPES = ["https://www.googleapis.com/auth/gmail.send"]
+
+def create_token(SCOPE):
+    flow = InstalledAppFlow.from_client_secrets_file(
+        os.path.join('creds', "credentials.json"), SCOPES
+    )
+    creds = flow.run_local_server(port=0)
+
+    # Save token
+    with open(os.path.join('creds', "token.json"), "w") as f:
+        f.write(creds.to_json())
+
+    print("Token saved to creds/token.json")
+
+def send_email(sender:str, receiver:str, body:str, subject:str):
+    if 'token.json' not in os.listdir('creds'):
+        create_token(SCOPES)
+
+    url = "https://gmail.googleapis.com/gmail/v1/users/me/messages/send"
+
+    # Load token
+    creds = Credentials.from_authorized_user_file(os.path.join('creds', "token.json"))
+
+    message = MIMEMultipart('alternative')
+    message["From"] = sender
+    message["To"] = ", ".join(receiver)
+    message["Subject"] = subject
+    message.attach(MIMEText(body, 'html'))
+
+    raw_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
+
+    headers = {
+        "Authorization": f"Bearer {creds.token}",
+        "Content-Type": "application/json",
+    }
+
+    data = {"raw": raw_message}
+
+    proxies = {
+        "http": FPTS_PROXY,
+        "https": FPTS_PROXY,
+    }
+
+    response = requests.post(
+        url,
+        headers=headers,
+        data=json.dumps(data),
+        proxies=proxies,
+    )
+
+# Test Email
+# current_date = "20-11-2025"
+# html_res = "Table Goes Here"
+# send_email(
+#             sender=NOTI_EMAIL,
+#             receiver=RECEIPANT,
+#             subject=f"Interbank rate report | {current_date}",
+#             body=f"""
+# <html>
+#   <body>
+#     <h3>Crawler Report for {current_date}</h3>
+#     {html_res}
+#   </body>
+# </html>
+# """
+# )
